db8d1662474cd005fb6471553c1147a3c19736f6
Marko Smiljic <markosmiljic99@gmail.com>
2026-08-25 12:09:27 +0200

    refactor: optimize system ticking

--- engine/src/ecs/world.cpp
+++ engine/src/ecs/world.cpp
@@ -3,19 +3,17 @@
 
 void World::update()
 {
-	for (const auto& system : m_systems) {
-		if (system->getPhase() == SystemPhase::Update) {
-			system->tick(App::getInstance().getTime().getDeltaTime());
-		}
+	auto& systems = m_systems.at(SystemPhase::Update);
+	for (const auto& system : systems) {
+		system->tick(App::getInstance().getTime().getDeltaTime());
 	}
 }
 
 void World::render()
 {
-	for (const auto& system : m_systems) {
-		if (system->getPhase() == SystemPhase::Render) {
-			system->tick(App::getInstance().getTime().getDeltaTime());
-		}
+	auto& systems = m_systems.at(SystemPhase::Render);
+	for (const auto& system : systems) {
+		system->tick(App::getInstance().getTime().getDeltaTime());
 	}
 }
 
